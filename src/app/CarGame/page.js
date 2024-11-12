a126fc2ecbb621bdbcb677d323c50dbbe9cf1d2d
Sanoj Aminda <134132126+DonSanoj@users.noreply.github.com>
2024-11-13 02:55:14 +0530

    Update page.js

--- src/app/CarGame/page.js
+++ src/app/CarGame/page.js
@@ -3,6 +3,7 @@ import CarGame from "@/components/CarGame/CarGame";
 export default function carGame() {
     return (
         <div>
+            //Car gmae
             <CarGame />
         </div>
     );
